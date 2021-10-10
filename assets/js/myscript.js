5e4ba9eb440a9a76d3e5afeb75bc31a765d6c4a7
Jayson <Jesoria517@gmail.com>
2021-10-10 13:19:41 +0800

    add

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,1 +1,21 @@
-alert("hey");
+/**
+ * Map Function
+ * @type {Array}
+ */
+var rooms = ["h1", "h2", "h3"];
+var newRooms = rooms.map(functionMap);
+
+function functionMap(rm)
+{
+	if(rm == "h3")
+	{
+		return "h4";
+	}
+	else
+	{
+		return rm;
+	}
+}
+
+console.log(rooms);
+console.log(newRooms);
