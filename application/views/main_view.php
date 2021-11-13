0cf299b9cff23742327446c4c11900f8919f4fcf
Jayson <jaysonsoria517@gmail.com>
2021-11-13 11:24:46 +0800

    m

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -3,6 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 
 <?php $this->load->view('main_view/header'); ?>
+
 	<h1>Main View</h1>
 
 <?php $this->load->view('main_view/footer'); ?>
